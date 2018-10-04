9787091b7b478e0e58a39a3b37d14d004021e2d4
AndreevSemen <43631421+AndreevSemen@users.noreply.github.com>
2018-10-04 11:42:37 +0300

    Update lab-01.cpp

--- lab-01.cpp
+++ lab-01.cpp
@@ -8,23 +8,23 @@
 
 int main()
 {
-	//Task#1
+	// Task#1
 	std::cout << 1 << " " << 13 << " " << 49 << std::endl;
 
 
-	//Task#2
+	// Task#2
 	char sym;
 	std::cin >> sym;
 	std::cout << 1 << sym << 13 << sym << 49 << std::endl;
 
 
-	//Task#3
+	// Task#3
 	int int1, int2, int3;
 	std::cin >> int1 >> int2 >> int3;
 	std::cout << int1 << "  " << int2 << "  " << int3 << std::endl;
 
 
-	//Task#4
+	// Task#4
 	double a, x, y;
 	std::cin >> a;
 	x = 12*a*a + 7*a - 12;
@@ -34,21 +34,21 @@ int main()
 	std::cout << "y=" << y << std::endl;
 
 
-	//Task#5
+	// Task#5
 	double V, m, p;
 	std::cin >> V >> m;
 	p = m/V;
 	std::cout << "p=" << p << std::endl;
 
 
-	//Task#6
+	// Task#6
 	double b;
 	std::cin >> a >> b;
 	x = -b/a;
 	std::cout << "x=" << x;
 
 
-	//Task#7
+	// Task#7
 	double x1, y1, x2, y2, d;
 	std::cout << "A(x1, y1)";
 	std::cin >> x1 >> y1;
